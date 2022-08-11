8dd85a888c56bcd5291b424c7f4a56fff09e2e5f
Henry Romero <hromerp22@gmail.com>
2022-08-11 19:40:29 -0400

    creacion de requerimiento, tres campos: tipo texto, tipo selector, botón y muestra de alerta

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Input msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Input
   }
 }
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello"> 
+    <h1>{{ msg }}</h1>
+      <input
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingresa Nombre"
+      v-model="name"
+    />
+    <div class="form-group">
+    <label class="text-left" for="exampleFormControlSelect1">Elige un nùmero</label>
+    <select class="form-control" id="exampleFormControlSelect1" v-model="select">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select>
+  </div>
+        <button
+      class="btn btn-block mt-2 btn-dark"
+      type="submit"
+      v-on:click="procesar"
+    >
+      Procesar
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      name:'',
+      select:'',
+    }
+  },
+  methods:{
+    procesar(){
+      alert(`${this.name}, ${this.select}`)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
